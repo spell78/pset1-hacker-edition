f78b7519ce4e10d1fe1bc32a5dfec8d820d60537
Lev Blankfeld <les2265@mail.ru>
2015-03-24 21:28:07 +0300

    Update bad_credit.c

--- bad_credit.c
+++ bad_credit.c
@@ -12,36 +12,38 @@ int main(void)
     long long cc_num = 0;
     char cc_string[17];
     int length = 0;
-    int summ = 0;
+    int sum = 0;
+    int sum1 = 0;
+    int sum2 = 0;
     int temp_value = 0;
+    //askin user to input credit card number
     
-    //askin user to input a credit card number
     do
     {
         printf("Number:");
         cc_num = GetLongLong();
     }
-    while (cc_num != 13 || cc_num != 15 || != 16);// maybe i need to set digits into ''
+    while (cc_num < 1);
     
-    //converting number into string
-    sprintf(cc_string, %llu, cc_num);
+    sprintf(cc_string, "%llu", cc_num);
     length = strlen(cc_string);
     
-    //starting from second to last digit
-    for (int i = length - 1; i >= 0; i-=2)
+    for (int i = length - 2; i >= 0; i-=2)
     {
         temp_value = (cc_string[i] - 48) * 2;
         if (temp_value < 10)
         {
-            sum = sum + temp_value;
+            sum1 = sum1 + temp_value;
         }
         else
         {
-            sum = sum + 1 + (temp_value - 10);//add digits together if product is >10 (if 14 than 1+4)
-        }
-        for (i = length; i >= 0; i-=2)//add the sum of each other element started from non-second to last
-        {
-            sum = sum + (cc_string[i] - 48);
+            sum1 = sum1 + 1 + (temp_value - 10);//add digits together if product is >10 (if 14 than 1+4)
         }
     }
+    for (int j = length-1; j >= 0; j-=2)//add the sum of each other element started from non-second to last
+    {
+        sum2 = sum2 + (cc_string[j] - 48);
+    }
+    sum = sum1 + sum2;
+    printf("%i\n", sum);       
 }
