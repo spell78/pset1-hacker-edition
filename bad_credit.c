42ac66d7985b38c532b8f995efb7577619f60a6d
Lev Blankfeld <les2265@mail.ru>
2015-03-24 15:12:08 +0300

    Update bad_credit.c

--- bad_credit.c
+++ bad_credit.c
@@ -14,16 +14,20 @@ int main(void)
     int length = 0;
     int summ = 0;
     int temp_value = 0;
+    
+    //askin user to input a credit card number
     do
     {
         printf("Number:");
         cc_num = GetLongLong();
     }
-    while (cc_num != 13 || cc_num != 15 || != 16);
+    while (cc_num != 13 || cc_num != 15 || != 16);// maybe i need to set digits into ''
     
+    //converting number into string
     sprintf(cc_string, %llu, cc_num);
     length = strlen(cc_string);
     
+    //starting from second to last digit
     for (int i = length - 1; i >= 0; i-=2)
     {
         temp_value = (cc_string[i] - 48) * 2;
@@ -33,9 +37,9 @@ int main(void)
         }
         else
         {
-            sum = sum + 1 + (temp_value - 10);
+            sum = sum + 1 + (temp_value - 10);//add digits together if product is >10 (if 14 than 1+4)
         }
-        for (i = length; i >= 0; i-=2)
+        for (i = length; i >= 0; i-=2)//add the sum of each other element started from non-second to last
         {
             sum = sum + (cc_string[i] - 48);
         }
